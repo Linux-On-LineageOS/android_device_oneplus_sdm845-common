287dc891fafa297098a2c3caa4a4e2755b8c91af
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:34:43 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,3 +167,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
